0a1e37a2810b7361e284ae474296c718b4cc7832
MishaHudz <misha.hudz@gmail.com>
2023-07-10 19:46:52 +0300

    project init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-
 import HomePage from 'pages/HomePage/HomePage';
 import Page from 'pages/Page/Page';
 import Layout from './Layout/Layout';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
